46f2f8840252523d0d8549ddfa2a55f37aa7b75e
Carlo Fernando <ca.fernando415@gmail.com>
2018-10-09 23:41:52 +0000

    automatically backed up by learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,7 +1,7 @@
 def oxford_comma(array)
   solution = ""
   if array.size == 1
-    solution = "#{array}"
+    solution = array[0]
   elsif array.size == 2
     solution = array.first + " and " + array.last
   else
