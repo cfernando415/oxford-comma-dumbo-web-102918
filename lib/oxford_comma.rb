bb9a966f62ce38b6aa91adde4be4d1cae13e1127
Carlo Fernando <ca.fernando415@gmail.com>
2018-10-09 23:34:08 +0000

    automatically backed up by learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -4,6 +4,13 @@ def oxford_comma(array)
     puts array
   elsif array.size == 2
     solution = array.first + " and " + array.last
-    
+  else
+    array.each do |item|
+      if item == array.last
+        solution << ", and " + item
+      else
+        solution << item + ", "
+      end
+    end
   end
 end
