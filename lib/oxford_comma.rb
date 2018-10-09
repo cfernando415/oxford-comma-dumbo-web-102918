daa3d2603822b331d76dd94252122fb2ecd2d4b3
Carlo Fernando <ca.fernando415@gmail.com>
2018-10-09 23:30:33 +0000

    automatically backed up by learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -3,7 +3,7 @@ def oxford_comma(array)
   if array.size == 1
     puts array
   elsif array.size == 2
-    solution = array.first
+    solution = array.first + " and " + array.last
     
   end
 end
